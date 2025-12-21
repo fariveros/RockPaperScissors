089adc38556f342fc68af9c142aa72136bc3449e
fariveros <felixandresriveros@gmail.com>
2025-12-21 13:46:55 -0500

    Hacer funcion para obtener la elección del pc. Hacer función para obtener la elección del usuario. Hacer función para saber quien gana entre el usuario y el computador y sumarle un punto

---
+++ game.js
@@ -0,0 +1,65 @@
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
+    let eleccionPc = Math.floor(Math.random() * (4 - 1) + 1)
+    switch (eleccionPc) {
+        case 1:
+            return "piedra"
+            break
+        case 2:
+            return "papel"
+            break
+        case 3:
+            return "tijera"
+            break
+
+    }
+}
+function getHumanChoice() {
+    let eleccion = prompt('Ingrese "Piedra" "Papel" o "Tijera"')
+    let elegido = eleccion.toLowerCase()
+    switch (elegido) {
+        case "piedra":
+            return "piedra"
+            break
+        case "papel":
+            return "papel"
+            break
+        case "tijera":
+            return "tijera"
+            break
+    }
+}
+function playRound(humanChoice, computerChoice) {
+
+    if(humanChoice === computerChoice) {
+        console.log("Haz empatado")
+    }
+    else if (humanChoice === "piedra" && computerChoice === "tijera") {
+        console.log("Haz ganado esta ronda") 
+        ++humanScore
+    }
+    else if (humanChoice === "papel" && computerChoice === "piedra") {
+        console.log("Haz ganado esta ronda") 
+        ++humanScore
+    }
+    else if (humanChoice === "tijera" && computerChoice === "papel") {
+        console.log("Haz ganado esta ronda")
+        ++humanScore
+    }
+    else {
+        console.log("Haz perdido esta ronda")
+        ++computerScore
+    }
+}
+function playGame () {
+    
+}
+const humanSelection = getHumanChoice();
+const com)puterSelection = getComputerChoice();
+console.log(`El valor seleccionado por el usuario es: ${humanSelection}`)
+console.log(`El valor seleccionado por el computador es: ${computerSelection}`)
+playRound(humanSelection, computerSelection)
+console.log(humanScore)
+console.log(computerScore)
